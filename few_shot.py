0a6132e248ebf9ad5be864535c266f80485e739f
Adarsh <>
2026-03-28 12:07:25 +0530

    stroing data now into Pandas

---
+++ few_shot.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+
+
+class FewShotPosts:
+    def __init__(self, file_path="data/processed_posts.json"):
+        self.df = None
+        self.unique_tags = None
+        self.load_posts(file_path)
+
+    def load_posts(self, file_path):
+        with open(file_path, encoding="utf-8") as f:
+            posts = json.load(f)
+            df = pd.json_normalize(posts)
+            df['length'] = df['line_count'].apply(self.categorize_length)
+            self.df = df
+
+    def categorize_length(self, line_count):
+        if line_count < 5:
+            return "Short"
+        elif 5 <= line_count <= 10:
+            return "Medium"
+        else:
+            return "Long"
+
+if __name__ == "__main__":
+    fs = FewShotPosts()
